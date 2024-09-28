3dcf91d102ae64a94aee065eadf7598e93abeb8b
barakadanny <barakadan421@gmail.com>
2024-09-28 11:26:31 +0100

    Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "./../components/Layout/Layout";
+
+const Home = () => {
+  return (
+    <Layout>
+      <div className="text-3xl font-bold underline">Hello world</div>
+    </Layout>
+  );
+};
+
+export default Home;
